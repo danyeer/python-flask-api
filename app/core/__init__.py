916c57299826e43d551236fd645cfbf0b8471dae
danyeer <danyeer@sina.cn>
2020-08-26 18:01:06 +0800

    del

--- app/core/__init__.py
+++
@@ -1,5 +0,0 @@
-# _*_ coding: utf-8 _*_
-"""
-  Created by Allen7D on 2020/4/19.
-"""
-__author__ = 'Allen7D'
--- app/extensions/api_docs/v1/pay.py
+++
@@ -1,5 +0,0 @@
-# _*_ coding: utf-8 _*_
-"""
-  Created by Allen7D on 2019/4/4.
-"""
-__author__ = 'Allen7D'
